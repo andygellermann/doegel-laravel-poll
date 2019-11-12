5b267885b2e2b0cb9b9e6e037e855467f7cd5449
Andy Gellermann <andy@gellermann.berlin>
2019-11-12 13:05:02 +0100

    fix cookieless voting problems

--- resources/views/umfrage/index.blade.php
+++ resources/views/umfrage/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="m-4">
         <p>Vielen Dank für Ihr Interesse an äußerst raffinierten Umfragen und deren fantastische Welt der Administration!</p>
-        <p>Diese Seite wurde komplett mit Laravel sowie mit dem unaufregendem Design-Framework Bootstrap entwickelt und benötigt für den öffentlichen Bereich keine Cookies (wenngleich die Laravel-App diese auf Nutzerseite speichert). Stattdessen wird zur Eindämmung von mehrfach-Abstimmungen die "sogenannte" Fingerprint-Technologie verwendet. Sicherlich ist diese Technik mehr ein Schlupfloch als gesetzlich akzeptierte Technologie. Aber schauen Sie selbst.</p>
+        <p>Diese Seite wurde komplett mit Laravel sowie mit dem unaufregendem Design-Framework Bootstrap entwickelt und benötigt für den öffentlichen Bereich (fast) keine Cookies (wenngleich die Laravel-App diese auf Nutzerseite speichert). Stattdessen wird zur Eindämmung von mehrfach-Abstimmungen zusätzlich die "sogenannte" Fingerprint-Technologie verwendet. Sicherlich ist diese Technik mehr ein Schlupfloch als gesetzlich akzeptierte Technologie. Aber schauen Sie selbst.</p>
         <p>Ich wünsche Ihnen maximale Neugier und Spaß!</p>
         <p>Ihr Andy Gellermann</p>
     </div>
